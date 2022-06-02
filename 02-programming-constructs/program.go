26fbdf30117c04397438457ed9622d220c03be0d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-02 10:22:11 +0530

    control flow - complete

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -58,4 +58,112 @@ LOOP:
 			}
 		}
 	}
+
+	fmt.Println("switch case")
+	/*
+		score 0-3 => "Terrible"
+		score 4-7 => "Not Bad"
+		score 8-9 => "Very Good"
+		score 10 => "Excellent"
+		otherwise => "Invalid score"
+	*/
+	score := 7
+	/*
+		switch score {
+		case 0:
+			fmt.Println("Terrible")
+		case 1:
+			fmt.Println("Terrible")
+		case 2:
+			fmt.Println("Terrible")
+		case 3:
+			fmt.Println("Terrible")
+		case 4:
+			fmt.Println("Not Bad")
+		case 5:
+			fmt.Println("Not Bad")
+		case 6:
+			fmt.Println("Not Bad")
+		case 7:
+			fmt.Println("Not Bad")
+		case 8:
+			fmt.Println("Very Good")
+		case 9:
+			fmt.Println("Very Good")
+		case 10:
+			fmt.Println("Excellent")
+		default:
+			fmt.Println("Invalid score")
+		}
+	*/
+
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6, 7:
+		fmt.Println("Not Bad")
+	case 8, 9:
+		fmt.Println("Very Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid score")
+	}
+
+	no := 12
+	switch {
+	case no%3 == 0:
+		fmt.Printf("%d is divisible by 3\n", no)
+	case no%4 == 0:
+		fmt.Printf("%d is divisible by 4\n", no)
+	}
+
+	fmt.Println("switch (with fallthrough)")
+	n := 4
+	switch n {
+	case 0:
+		fmt.Println("is zero")
+	case 1:
+		fmt.Println("n <= 1")
+		fallthrough
+	case 2:
+		fmt.Println("n <= 2")
+		fallthrough
+	case 3:
+		fmt.Println("n <= 3")
+		fallthrough
+	case 4:
+		fmt.Println("n <= 4")
+		fallthrough
+	case 5:
+		fmt.Println("n <= 5")
+		fallthrough
+	case 6:
+		fmt.Println("n <= 6")
+		fallthrough
+	case 7:
+		fmt.Println("n <= 7")
+		//fallthrough
+	case 8:
+		fmt.Println("n <= 8")
+		fallthrough
+	case 9:
+		fmt.Println("n <= 9")
+	}
+
+	subscription := "pro"
+	switch subscription {
+	case "premium":
+		fmt.Println("All <premium> features are included")
+		fallthrough
+	case "pro":
+		fmt.Println("All <pro> features are included")
+		fallthrough
+	case "basic":
+		fmt.Println("All <basic> features are included")
+		fallthrough
+	case "free":
+		fmt.Println("All <free> features are included")
+
+	}
 }
