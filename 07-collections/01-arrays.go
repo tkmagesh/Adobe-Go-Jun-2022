33d692f45c21af3bf47c08cbd58170432f2464ee
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-03 11:46:46 +0530

    arrays

---
+++ 07-collections/01-arrays.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var nos [5]int
+	//var nos [5]int = [5]int{3, 1, 4, 2, 5}
+	//var nos [5]int = [5]int{3, 1, 4}
+	//nos := [5]int{}
+	nos := [5]int{3, 1, 4, 2, 5}
+	fmt.Println(nos)
+
+	//iterating (using indexer)
+	fmt.Println("iterating (using indexer)")
+	for i := 0; i < len(nos); i++ {
+		fmt.Printf("nos[%d] = %d\n", i, nos[i])
+	}
+
+	//iterating (using range)
+	fmt.Println("iterating (using range)")
+	for idx, val := range nos {
+		fmt.Printf("nos[%d] = %d\n", idx, val)
+	}
+
+	//iterating (using range) (only values)
+	fmt.Println("iterating (using range)")
+	for _, val := range nos {
+		fmt.Println(val)
+	}
+}
