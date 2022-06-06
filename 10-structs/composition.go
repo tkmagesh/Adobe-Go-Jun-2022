0208e4c07405f447fc3ef3cf698ec0524de9c1d3
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-06 12:54:05 +0530

    composition (aka inheritance)

---
+++ 10-structs/composition.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Units    int
+	Category string
+}
+
+type PerishableProduct struct {
+	Product
+	Expiry string
+}
+
+func NewPerishableProduct(id int, name string, cost float32, units int, category string, expiry string) PerishableProduct {
+	return PerishableProduct{Product{id, name, cost, units, category}, expiry}
+}
+
+func main() {
+	/* var grapes = PerishableProduct{
+		Product: Product{
+			Id:       200,
+			Name:     "Grapes",
+			Cost:     40,
+			Units:    2,
+			Category: "Food",
+		},
+		Expiry: "2 DAYS",
+	} */
+
+	var grapes = NewPerishableProduct(200, "Grapes", 40, 2, "Food", "2 DAYS")
+
+	fmt.Println(grapes.Product.Name)
+	//fmt.Println(grapes.Name)
+}
