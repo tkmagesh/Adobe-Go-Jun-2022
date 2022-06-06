4bf2c77aaa03543607a565abe5116d6d84d49f27
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-06 13:17:09 +0530

    Update composition.go

--- 10-structs/composition.go
+++ 10-structs/composition.go
@@ -15,8 +15,8 @@ type PerishableProduct struct {
 	Expiry string
 }
 
-func NewPerishableProduct(id int, name string, cost float32, units int, category string, expiry string) PerishableProduct {
-	return PerishableProduct{Product{id, name, cost, units, category}, expiry}
+func NewPerishableProduct(id int, name string, cost float32, units int, category string, expiry string) *PerishableProduct {
+	return &PerishableProduct{Product{id, name, cost, units, category}, expiry}
 }
 
 func main() {
@@ -34,17 +34,21 @@ func main() {
 	var grapes = NewPerishableProduct(200, "Grapes", 40, 2, "Food", "2 DAYS")
 
 	//fmt.Println(grapes.Product.Name)
-	fmt.Println(Foramt(grapes)) //=> Id = 100, Name = "Grapes", Cost = 40, Units = 2, Category = "Food", Expiry = "2 DAYS"
+	fmt.Println(Format(*grapes)) //=> Id = 100, Name = "Grapes", Cost = 40, Units = 2, Category = "Food", Expiry = "2 DAYS"
 
-	ApplyDiscount() //APPLY 10% DISCOUNT FOR GRAPES
+	ApplyDiscount(grapes, 10) //APPLY 10% DISCOUNT FOR GRAPES
 
-	fmt.Println(Foramt(grapes)) //=> Id = 100, Name = "Grapes", Cost = 36, Units = 2, Category = "Food", Expiry = "2 DAYS"
+	fmt.Println(Format(*grapes)) //=> Id = 100, Name = "Grapes", Cost = 36, Units = 2, Category = "Food", Expiry = "2 DAYS"
 }
 
-func Format( /*  */ ) string {
-
+func FormatProduct(p Product) string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %v, Units = %d, Category = %q", p.Id, p.Name, p.Cost, p.Units, p.Category)
 }
 
-func ApplyDiscount( /*  */ ) {
+func Format(pp PerishableProduct) string {
+	return fmt.Sprintf("%v, Expiry = %q", FormatProduct(pp.Product), pp.Expiry)
+}
 
+func ApplyDiscount(pp *PerishableProduct, discount float32) {
+	pp.Cost = pp.Cost * ((100 - discount) / 100)
 }
