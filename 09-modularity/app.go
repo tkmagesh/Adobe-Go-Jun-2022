e0cd1dabe9f09fbbe616c1cdc7f402ad07261921
Magesh Kuppan <tkmagesh77@gmail.com>
2022-06-06 11:35:42 +0530

    modularity (modules & packages)

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-	_ "modularity-demo/calculator" //to execute the init function without using any of the apis
+	"modularity-demo/calculator"
+
+	//_ "modularity-demo/calculator" //to execute the init function without using any of the apis
 	myUtils "modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
 	fmt.Println("Modularity demo")
-	/*
-		fmt.Println(calculator.Add(100, 200))
-		fmt.Println(calculator.Subtract(100, 200))
-		fmt.Println("OpCount = ", calculator.OpCount())
-	*/
+
+	fmt.Println(calculator.Add(100, 200))
+	fmt.Println(calculator.Subtract(100, 200))
+	fmt.Println("OpCount = ", calculator.OpCount())
 
 	//fmt.Println("Is 21 an even number ? :", utils.IsEven(21))
 	fmt.Println("Is 21 an even number ? :", myUtils.IsEven(21))
+
+	color.Red("This line is printed in red color")
+	color.Blue("This line is printed in blue color")
+
 }
